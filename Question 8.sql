e2f0ec9d61e93ac4c409b32b23c4b6f10fd564bc
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-08-09 01:21:02 +0530

    Add files via upload

---
+++ Question 8.sql
@@ -0,0 +1,7 @@
+
+# 16. What are the top marketing ways we can market our CodeX energy drink?
+select a.marketing_channels,b.marketing_sub_types as marketing_way,b.rating from fact_survey_responses a
+join marketing_types b on b.marketing_channels=a.marketing_channels
+where b.rating in (4,5)
+group by 1,2,3
+order by 3 desc; 
